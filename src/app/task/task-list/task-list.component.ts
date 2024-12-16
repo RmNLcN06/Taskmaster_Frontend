173e33f69183d71dabea3368672eba209897407a
RmNLcN06 <romain.luciano@orange.fr>
2024-12-16 15:52:37 +0100

    Create deleteTask method in component.ts & implement in component.html

--- src/app/task/task-list/task-list.component.ts
+++ src/app/task/task-list/task-list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Task } from '../task.model';
 import { DatePipe } from '@angular/common';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-task-list',
@@ -9,43 +10,22 @@ import { DatePipe } from '@angular/common';
   styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent {
-  tasks: Task[] = [
-    {
-      id: 1,
-      name: "Design wireframe",
-      description: "",
-      completed: false,
-      dueDate: new Date("2024-12-15"),
-      project: 1
-    },
-    {
-      id: 2,
-      name: "Develop Frontend",
-      description: "",
-      completed: true,
-      dueDate: new Date("2024-08-11"),
-      project: 1
-    },
-    {
-      id: 3,
-      name: "Implement Backend",
-      description: "",
-      completed: false,
-      dueDate: new Date("2024-12-13"),
-      project: 1
-    },
-    {
-      id: 4,
-      name: "Test Frontend",
-      description: "",
-      completed: true,
-      dueDate: new Date("2024-11-18"),
-      project: 1
-    },
-  ];
+  tasks: Task[];
 
+  private taskService = inject(TaskService);
+
+  constructor() {
+    this.tasks = this.taskService.getTasks();
+  }
+  
   handleCheckbox(id: number) {
     const taskIndex = this.tasks.findIndex((task) => task.id === id);
-    this.tasks[taskIndex].completed = !this.tasks[taskIndex].completed;
+    const updatedTask = this.tasks[taskIndex];
+    updatedTask.completed = !updatedTask.completed;
+    this.tasks = this.taskService.updateTask(updatedTask);
+  }
+
+  deleteTask(id: number) {
+    this.tasks = this.taskService.deleteTask(id);
   }
 }

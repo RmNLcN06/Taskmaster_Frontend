3c6d74fd2310f8f484071b7104f8e012738d8c08
RmNLcN06 <romain.luciano@orange.fr>
2024-12-16 11:37:06 +0100

    Making task-list component dynamic

--- src/app/task/task-list/task-list.component.ts
+++ src/app/task/task-list/task-list.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { Task } from '../task.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent {
-
+  tasks: Task[] = [
+    {
+      id: 1,
+      name: "Design wireframe",
+      description: "",
+      completed: false,
+      dueDate: new Date("2024-12-15"),
+      project: 1
+    },
+    {
+      id: 2,
+      name: "Develop Frontend",
+      description: "",
+      completed: true,
+      dueDate: new Date("2024-08-11"),
+      project: 1
+    },
+    {
+      id: 3,
+      name: "Implement Backend",
+      description: "",
+      completed: false,
+      dueDate: new Date("2024-12-13"),
+      project: 1
+    },
+    {
+      id: 4,
+      name: "Test Frontend",
+      description: "",
+      completed: true,
+      dueDate: new Date("2024-11-18"),
+      project: 1
+    },
+  ]
 }

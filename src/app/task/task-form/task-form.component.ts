ccec41eaa503c481fe9e00b37830ad7847984e5a
RmNLcN06 <romain.luciano@orange.fr>
2024-12-17 17:22:58 +0100

    Add to showModal method the close action when the modal is valid.

--- src/app/task/task-form/task-form.component.ts
+++ src/app/task/task-form/task-form.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Task } from '../task.model';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.scss'
 })
 export class TaskFormComponent {
+  @Output() closePanel = new EventEmitter<'SUBMIT'>();
+  taskForm: FormGroup;
 
+  private taskService = inject(TaskService);
+
+  constructor(private formBuilder: FormBuilder) {
+    this.taskForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      dueDate: ['', Validators.required],
+      id: [0],
+      project: [0],
+    });
+  }
+
+  handleSubmit() {
+    if(this.taskForm.valid) {
+      const newTask: Task = {
+        ...this.taskForm.value,
+        dueDate: new Date(this.taskForm.value.dueDate),
+        completed: false,
+      };
+      this.taskService.addTask(newTask);
+      this.closePanel.emit('SUBMIT');
+    }
+  }
 }
